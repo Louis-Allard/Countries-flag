ae40b55394cd2349ab6f2ba3c4ea207daa6427ee
Louis Allard <la.devweb@yahoo.com>
2021-09-08 15:57:12 +0200

    Add

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,9 +9,9 @@ const About = () => {
             <Logo />
             <h1>A propos</h1>
             <br />
-            <p>Qui in officia minim eiusmod. Deserunt magna est sit exercitation est magna ex. Ut duis proident labore excepteur non voluptate in officia amet adipisicing. Minim velit non sit sit pariatur occaecat do enim. Irure ex deserunt consequat commodo mollit.</p>
-            <br />
-            <p>Mollit sunt in ex sunt. Incididunt ea consectetur ullamco ea do eu non nostrud consectetur excepteur enim. Fugiat quis nostrud excepteur proident minim cupidatat qui in ipsum magna duis Lorem. Laboris minim dolor eiusmod id amet officia dolore commodo consectetur ad nostrud anim. Pariatur nulla aute aute amet irure. Lorem aliquip ex dolore ut eu dolor eiusmod magna amet duis incididunt. Culpa eu culpa Lorem ullamco qui officia minim nisi amet in et mollit cupidatat.</p>
+            <p>la.devweb@yahoo.com</p>
+            <p>http://www.louisallard.fr</p>
+            <p>Made with love and React JS</p>
         </div>
     );
 };
